1cacae45d8adb2980becc65ee0dff62b44e858a7
Cat61 <catherine.fowler@student.manchester.ac.uk>
2015-07-21 22:33:44 +0100

    Welcome Message added

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,14 @@
 package com.twu.biblioteca;
 
-public class BibliotecaApp {
+public class BibliotecaApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println(welcome());
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+    protected static String welcome()
+    {
+        return "Welcome!";
     }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaAppTest
+{
+    @Test
+    public void testWelcome()
+    {
+        String expected = "Welcome!";
+        String actual = BibliotecaApp.welcome();
+        assertEquals(expected, actual);
+    }
+}
