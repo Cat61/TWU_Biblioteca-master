2055e77d16f8fc5a8a91e23dee0dc745e2471ca0
Cat61 <catherine.fowler@student.manchester.ac.uk>
2015-07-25 20:58:26 +0100

    Added tests for Book
    For EqualsTitle
    For Successful Return
    For Unsuccessful Return

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -17,7 +17,7 @@ public class BookTest
     }
 
     @Test
-    public void testCheckoutAv()
+    public void testCheckoutAvailable()
     {
         boolean success = book.checkout();
         assertTrue(success);
@@ -26,7 +26,7 @@ public class BookTest
     }
 
     @Test
-    public void testCheckoutNotAv()
+    public void testCheckoutNotAvailable()
     {
         boolean success = book.checkout();
         assertTrue(success);
@@ -43,5 +43,36 @@ public class BookTest
         String expected = "Effective Java                      Joshua Bloch                        2001";
         String actual = book.toString();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testEqualsTitle()
+    {
+        assertTrue(book.equalsTitle("Effective Java"));
+
+        assertFalse(book.equalsTitle("liwug"));
+
+        assertFalse(book.equalsTitle("effective Java"));
+    }
+
+    @Test
+    public void testSuccessfulReturnBook()
+    {
+        boolean success = book.checkout();
+        assertTrue(success);
+
+        success = book.returnBook();
+        assertTrue(success);
+
+        assertTrue(book.isAvailable());
+    }
+
+    @Test
+    public void testUnsuccessfulReturnBookAvailable()
+    {
+        boolean success = book.returnBook();
+        assertFalse(success);
+
+        assertTrue(book.isAvailable());
     }
 }
