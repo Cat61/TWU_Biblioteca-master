ac3ff988c52b16ec0b8b4761881333057e809374
Cat61 <catherine.fowler@student.manchester.ac.uk>
2015-07-25 19:25:19 +0100

    Updated tests to checkout

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -62,7 +62,12 @@ public class BibliotecaAppTest
 
         BibliotecaApp.checkoutBook("3");
 
-        String expected = "\n\nBook List:\n" +
+        String expected = "\nThank you! Enjoy the book.\n\n";
+        String actual = getTerminalOutput();
+        assertEquals(expected, actual);
+
+        outContent.reset();
+        expected = "\nBook List:\n" +
                 "   Title                               Author                              Year\n" +
                 "-------------------------------------------------------------------------------\n" +
                 "(1)Head First Java                     Kathy Sierra, Bert Bates            2005\n" +
@@ -71,7 +76,7 @@ public class BibliotecaAppTest
                 "(4)Java All-in-One For Dummies         Doug Lowe                           2014\n" +
                 "(5)Learning Java                       Patrick Niemeyer, Daniel Leuck      2013\n\n";
         BibliotecaApp.selectMenuOption("l");
-        String actual = getTerminalOutput();
+        actual = getTerminalOutput();
         assertEquals(expected, actual);
     }
 
@@ -82,8 +87,12 @@ public class BibliotecaAppTest
 
         BibliotecaApp.checkoutBook("8");
 
-        String expected = "\nUnsuccessful checkout!\n\n" +
-                "Book List:\n" +
+        String expected = "\nUnsuccessful checkout!\n\n";
+        String actual = getTerminalOutput();
+        assertEquals(expected, actual);
+
+        outContent.reset();
+        expected ="\nBook List:\n" +
                 "   Title                               Author                              Year\n" +
                 "-------------------------------------------------------------------------------\n" +
                 "(1)Head First Java                     Kathy Sierra, Bert Bates            2005\n" +
@@ -93,7 +102,7 @@ public class BibliotecaAppTest
                 "(5)Java All-in-One For Dummies         Doug Lowe                           2014\n" +
                 "(6)Learning Java                       Patrick Niemeyer, Daniel Leuck      2013\n\n";
         BibliotecaApp.selectMenuOption("l");
-        String actual = getTerminalOutput();
+        actual = getTerminalOutput();
         assertEquals(expected, actual);
     }
 
